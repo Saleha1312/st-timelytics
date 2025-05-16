d28d1eaa1bf8da6c590ed872bac8dea480ec7936
Saleha1312 <salehabushra70@gmail.com>
2025-05-16 17:51:13 +0530

    ImageProject

--- converted_keras/cam.py
+++ converted_keras/cam.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import tensorflow as tf
+# import tensorflow as tf
 from keras.models import load_model  # TensorFlow is required for Keras to work
 import cv2  # Install opencv-python
 import numpy as np
